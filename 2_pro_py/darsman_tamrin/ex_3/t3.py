bac5ab0613060b7d4bd96be102ee883bcba2b231
Nekou <nekou.workcpe@gmail.com>
2023-11-22 23:41:17 +0330

    new commit

--- 2_pro_py/darsman_tamrin/ex_3/t3.py
+++ 2_pro_py/darsman_tamrin/ex_3/t3.py
@@ -7,6 +7,18 @@ class Product_Price():
         obj.sum=sum([product for product in obj.list_price])
         return self.sum<obj.sum
 
+    def __eq__(self, obj) -> bool:
+        list_bool_eq=[]
+        for product1 in self.list_price:
+            for product2 in obj.list_price:
+                if product1<product2:
+                    return False
+                elif product1>product2:
+                    return False
+                else:
+                    return True
+        re
+        pass
     def __add__(self):
         pass
 
@@ -22,5 +34,5 @@ Product_Price_List1=[5000,10000,15000,6000,25000,12000,14000,10000,7000,20000]
 Product_Price1=Product_Price(Product_Price_List1)
 Product_Price_List2=[4000,12000,16000,5000,22000,10000,16000,11000,5000,18000]
 Product_Price2=Product_Price(Product_Price_List2)
-print('',Product_Price1.__it__(Product_Price2))
+print(Product_Price1.__it__(Product_Price2))
 
