87ca0d2b4b221b625583916775657bfc7a6ac995
Nekou <nekou.workcpe@gmail.com>
2023-11-26 19:47:11 +0330

    update

--- 2_pro_py/season2/part8/test2.py
+++ 2_pro_py/season2/part8/test2.py
@@ -5,9 +5,10 @@ point: plus self you should add name of value in setter
 use value name withou __
 we use getter and setter for access to privite value with security'''
 class Person():
-    def __init__(self,name,idCode) -> None:
+    def __init__(self,name,idCode,id) -> None:
         self.name=name
         self.__idCode=idCode
+        self.__id=id
 
     @property
     def idCode(self):
@@ -17,6 +18,15 @@ class Person():
     def idCode(self,idCode):
         self.__idCode=idCode
     
+    @property
+    def id(self):
+        return self.__id+'*'
+    
+    @id.setter
+    def id(self,id):
+        self.__id=self.__id
 
-Person1=Person('Nekou','400130100')
+#instance
+Person1=Person('Nekou','400130100','NekouDfrm123')
 print(Person1.idCode)
+print(Person1.id)
